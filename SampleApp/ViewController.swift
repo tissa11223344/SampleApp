53e9bcae09b735b439542adaead4610094696861
Tissa <tissa@Tissas-MacBook-Pro.local>
2023-09-05 18:02:50 +0530

    first commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Tissa on 9/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
